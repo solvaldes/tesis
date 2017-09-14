8b5f49cad003b0e5b287f41649677a0dec9ba84e
Sol Valdes <solvaldes15@gmail.com>
2017-09-13 22:04:57 -0300

    Agregado de transiciones

--- Proyecto_Tesis/Proyecto_Tesis.ino
+++ Proyecto_Tesis/Proyecto_Tesis.ino
@@ -10,7 +10,30 @@
 PS2Mouse mouse(MOUSE_CLOCK, MOUSE_DATA, STREAM);
 PS2Mouse mouse2(MOUSE_CLOCK_2, MOUSE_DATA_2, STREAM);
 
+#define colorSaturation 128
+RgbColor red(colorSaturation, 0, 0);
+RgbColor green(0, colorSaturation, 0);
+RgbColor blue(0, 0, colorSaturation);
+RgbColor black(0);
+
+
+const int cantidadLeds= 4;
+struct LedsEstado{
+    int id;
+    int cantidad;
+    RgbColor color;
+    int leds[cantidadLeds];  
+};
+
 int cantidadRatonesVivos= 0;
+bool cantidadRatonesModificada= false;
+
+
+LedsEstado estadoLeds0= {0, cantidadLeds, black,{0,1,2,3}};
+LedsEstado estadoLeds1= {1, 2, blue, {0,1}};
+LedsEstado estadoLeds2= {2, cantidadLeds, green, {0,1,2,3}};
+
+LedsEstado estadoLedsActual= estadoLeds0;
 
 struct Raton { 
     int numero;
@@ -19,39 +42,29 @@ struct Raton {
     bool estaVivo= false;
 };
 
+
+
   Raton raton1;
   Raton raton2;
 
-#define colorSaturation 128
-RgbColor red(colorSaturation, 0, 0);
-RgbColor green(0, colorSaturation, 0);
-RgbColor blue(0, 0, colorSaturation);
-RgbColor black(0);
 
-const uint16_t PixelCount = 6; // make sure to set this to the number of pixels in your strip
+const uint16_t PixelCount = 8; // make sure to set this to the number of pixels in your strip
 const uint8_t PixelPin = 2;  // make sure to set this to the correct pin, ignored for Esp8266
-const uint8_t AnimationChannels = 1; // we only need one as all the pixels are animated at once
-
-NeoPixelBus<NeoGrbFeature, Neo800KbpsMethod> strip(PixelCount, PixelPin);
-// For Esp8266, the Pin is omitted and it uses GPIO3 due to DMA hardware use.  
-// There are other Esp8266 alternative methods that provide more pin options, but also have
-// other side effects.
-//NeoPixelBus<NeoGrbFeature, Neo800KbpsMethod> strip(PixelCount);
-//
-// NeoEsp8266Uart800KbpsMethod uses GPI02 instead
-
-NeoPixelAnimator animations(AnimationChannels); // NeoPixel animation management object
-
-uint16_t effectState = 0;  // general purpose variable used to store effect state
+const uint8_t AnimationChannels = PixelCount; // we only need one as all the pixels are animated at once
 
 
-// what is stored for state is specific to the need, in this case, the colors.
-// basically what ever you need inside the animation update function
+NeoPixelAnimator animations(PixelCount, NEO_CENTISECONDS);
 struct MyAnimationState
 {
-    RgbColor StartingColor;
-    RgbColor EndingColor;
+    RgbColor StartingColor;  // the color the animation starts at
+    RgbColor EndingColor; // the color the animation will end at
+    AnimEaseFunction Easeing; // the acceleration curve it will use 
 };
+
+
+NeoPixelBus<NeoGrbFeature, Neo800KbpsMethod> strip(PixelCount, PixelPin);
+uint16_t effectState = 0;  // general purpose variable used to store effect state
+
 
 // one entry per pixel to match the animation timing manager
 MyAnimationState animationState[AnimationChannels];  
@@ -108,51 +121,138 @@ void loopMice(){
   delay(300);  
 }
 
+void AnimUpdate(const AnimationParam& param)
+{
+
+  Serial.print("update");
+    // first apply an easing (curve) to the animation
+    // this simulates acceleration to the effect
+    float progress = animationState[param.index].Easeing(param.progress);
+
+    // this gets called for each animation on every time step
+    // progress will start at 0.0 and end at 1.0
+    // we use the blend function on the RgbColor to mix
+    // color based on the progress given to us in the animation
+    RgbColor updatedColor = RgbColor::LinearBlend(
+        animationState[param.index].StartingColor,
+        animationState[param.index].EndingColor,
+        progress);
+    // apply the color to the strip
+    strip.SetPixelColor(param.index, updatedColor);
+}
+
+void transicionarAEstado(LedsEstado estado, RgbColor color){
+    Serial.print(estado.id );
+    uint16_t time = 200;
+    for (int i=0; i < cantidadLeds; i++){
+
+        int pixel= estadoLeds0.leds[i];
+        RgbColor targetColor = black;
+        if (i < estado.cantidad){
+          for (int j=0; j < estado.cantidad; j++){
+            if (estado.leds[j] == pixel){
+              targetColor = color;  
+            }            
+          }
+          //pixel= estado.leds[i];
+          //targetColor = color;
+        }
+
+        // each animation starts with the color that was present
+        RgbColor originalColor = strip.GetPixelColor(pixel);
+        // and ends with a random color
+        
+        // with the random ease function
+        AnimEaseFunction easing;
+
+        switch (random(3))
+        {
+        case 0:
+            easing = NeoEase::CubicIn;
+            break;
+        case 1:
+            easing = NeoEase::CubicOut;
+            break;
+        case 2:
+            easing = NeoEase::QuadraticInOut;
+            break;
+        }
+
+#ifdef ARDUINO_ARCH_AVR        
+        animationState[pixel].StartingColor = originalColor;
+        // and ends with a random color
+        animationState[pixel].EndingColor = targetColor;
+        // using the specific curve
+        animationState[pixel].Easeing = easing;
+
+        // now use the animation state we just calculated and start the animation
+        // which will continue to run and call the update function until it completes
+        animations.StartAnimation(pixel, time, AnimUpdate);
+#else
+        AnimUpdateCallback animUpdate = [=](const AnimationParam& param)
+        {
+            // progress will start at 0.0 and end at 1.0
+            // we convert to the curve we want
+            float progress = easing(param.progress);
+
+            // use the curve value to apply to the animation            
+            RgbColor updatedColor = RgbColor::LinearBlend(originalColor, targetColor, progress);
+            strip.SetPixelColor(pixel, updatedColor);
+        };
+
+        // now use the animation properties we just calculated and start the animation
+        // which will continue to run and call the update function until it completes
+        animations.StartAnimation(pixel, time, animUpdate);        
+#endif        
+    }
+
+            animations.UpdateAnimations();
+        strip.Show();
+}
+
 void loopLeds()
 {
-  if (cantidadRatonesVivos == 0){
-    strip.SetPixelColor(0, black);
-    strip.SetPixelColor(1, black);
-    strip.SetPixelColor(2, black);
-    strip.SetPixelColor(3, black);
-    strip.SetPixelColor(4, black);
-    strip.SetPixelColor(5, black);
-  }  
+  if (cantidadRatonesModificada){    
+    
+    if (cantidadRatonesVivos == 0){
+      transicionarAEstado(estadoLeds0, black);
+    }    
   
-
-  if (cantidadRatonesVivos == 1){
-    strip.SetPixelColor(0, red);
-    strip.SetPixelColor(1, red);
-    strip.SetPixelColor(2, red);
-    strip.SetPixelColor(3, black);
-    strip.SetPixelColor(4, black);
-    strip.SetPixelColor(5, black);    
+    if (cantidadRatonesVivos == 1){    
+      transicionarAEstado(estadoLeds1, blue);
+    }
+  
+    if (cantidadRatonesVivos == 2){
+      transicionarAEstado(estadoLeds2, green);
+    }   
+  }
+  else{  
+    if (animations.IsAnimating()){
+       animations.UpdateAnimations();
+        strip.Show();
+    }
   }
 
-  if (cantidadRatonesVivos == 2){
-    strip.SetPixelColor(0, red);
-    strip.SetPixelColor(1, red);
-    strip.SetPixelColor(2, red);    
-    strip.SetPixelColor(3, green);
-    strip.SetPixelColor(4, green);
-    strip.SetPixelColor(5, green);
-  }  
+  cantidadRatonesModificada= false;
 
   
 
-  strip.Show();
+  
 }
+
 
 void procesarEstadoMouse(Raton *raton){
     if (hayMovimiento(raton->mx,raton->my)){
         if (!raton->estaVivo){
             cantidadRatonesVivos++;
+            cantidadRatonesModificada= true;
         }
         raton->estaVivo= true;
     }   
     else{
         if (raton->estaVivo){
             cantidadRatonesVivos--;
+            cantidadRatonesModificada= true;
         }
         raton->estaVivo= false;           
     } 
